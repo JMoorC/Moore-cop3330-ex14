c0319da5b85b6c2eb7724ae0795428d8ce24e878
Jim <jcmoore@knights.ucf.edu>
2021-09-13 13:42:23 -0400

    Initial commit

---
+++ src/main/java/ex14.java
@@ -0,0 +1,35 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 James Moore
+ */
+
+import java.util.Scanner;
+
+public class ex14 {
+
+    static final double WITAX = 0.055;
+
+    public static void main(String[] args){
+        Scanner inScan = new Scanner(System.in);
+
+        System.out.print("What is the order amount? ");
+        double inAmount = roundWholeCent(inScan.nextDouble());
+        System.out.print("What is the state? ");
+        String state = inScan.next().toLowerCase();
+
+        if(state.equals("wi") || state.equals("wisconsin")){
+            System.out.printf("The subtotal is $%.2f.\n", inAmount);
+            double tax = roundWholeCent(inAmount * WITAX);
+            System.out.printf("The tax is $%.2f.\n", tax);
+            inAmount += tax;
+        }
+
+        System.out.printf("The total is $%.2f.", inAmount);
+    }
+
+    public static double roundWholeCent(double value){
+        int hold = (int) (value * 1000000);
+        return Math.ceil( (hold / 10000.0) ) / 100.0;
+    }
+
+}
